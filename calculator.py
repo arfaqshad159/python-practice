eb9add4a478ce045deda5b4597fc320cac019584
Arfaqshad <arfaqshad159@gmail.com>
2026-07-23 14:46:30 +0530

    handle division by zero

---
+++ calculator.py
@@ -0,0 +1,14 @@
+print("Simple Calculator")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Product:", num1 * num2)
+
+if num2 == 0:
+    print("Quotient: Cannot divide by zero!")
+else:
+    print("Quotient:", num1 / num2)
+    
